9c8733577c4defb4b53ea9794dc6b740b3fdc2a8
chuanmaotou <cuemo.hsu@gmail.com>
2019-12-26 22:40:50 +0800

    Remove unused code.

--- src/components/TimelineAdmin/index.js
+++ src/components/TimelineAdmin/index.js
@@ -1,8 +1,7 @@
-import React, { useCallback } from 'react';
+import React from 'react';
 
 import { makeStyles } from '@material-ui/core/styles';
 import { Tooltip, Chip } from '@material-ui/core';
-import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import Divider from '@material-ui/core/Divider';
 import Typography from '@material-ui/core/Typography';
